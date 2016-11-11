a4cab57fc097ead37cc7c1f3dca08b7e233645a7
Cody Clawson <cclawson@student.neumont.edu>
2016-11-11 07:34:59 -0700

    changes to structure for server and html

--- app.js
+++ app.js
@@ -7,33 +7,35 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + "/public/index.html");
 });
 
-var currentMembers = [{}, {}, {}];
+var members = [];
 
 io.on("connection", function (socket) {
-    currentMembers[0] = socket;
-    currentMembers.forEach(function (member) {
-        console.log(member);
-    })
-    console.log(socket.id + " user has connected");
-    socket.nickname = 'Guest';
-    socket.on("disconnect", function () {
-        console.log(socket.id + "user has disconnected");
-    });
-
-    socket.on("chat message", function (msg) {
-        console.log(msg.msg);
-        io.emit('chat received', {
-            message: msg.msg,
-            sid: socket.id,
-            name: msg.name
+    if (members.length < 3) {
+        socket.on('adduser', function (username) {
+            socket.username = username;
+            members[username] = username;
+            socket.emit('updatechat', 'SERVER', 'you have connected');
+            socket.broadcast.emit('updatechat', 'SERVER', username + ' has connected');
+            io.sockets.emit('updateusers', members);
         });
-    });
 
-    socket.on("get clients", function (msg) {
-        console.log('sending clients');
-        console.log(io.sockets.clients());
-        io.emit('list clients', io.sockets.clients().adapter.sids);
-    });
+        socket.on('disconnect', function () {
+            delete members[socket.username];
+            io.sockets.emit('updateusers', members);
+            socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
+        });
+
+        socket.on('sendchat', function (data) {
+            io.sockets.emit('updatechat', socket.username, data);
+        });
+
+    } else {
+        console.log("too many players");
+        io.to(socket.id).emit('too Many', {
+            message: "We are sorry there are currently to many connections"
+        });
+        socket.disconnect();
+    }
 });
 
 app.use(express.static('public'));
