6b4e3a8c329062ce045891fc4c8a21268175df22
Cody Clawson <cclawson@student.neumont.edu>
2016-12-01 08:32:52 -0700

    change port number

--- app.js
+++ app.js
@@ -89,6 +89,15 @@ io.on("connection", function (socket) {
 
 app.use(express.static('public'));
 
-http.listen(3000, function () {
-    console.log("listening on port 3000");
-})
+var port = Number(process.env.PORT || 3000);
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
+
+exports = module.exports = app;
+
+// http.listen(3000, function () {
+//     console.log("listening on port 3000");
+// })
+
